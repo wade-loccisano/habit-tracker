d1fdbb0d34fc2970dedb718492275d0f2c1bd4e5
wade-loccisano <wade.loccisano@gmail.com>
2025-03-19 20:12:11 -0400

    Add a bunch of habit endpoints and the progress controller.

---
+++ api/src/API/Controllers/ProgressController.cs
@@ -0,0 +1,42 @@
+﻿using Application.UseCases.Progress.Queries;
+using Domain.DTOs.Output;
+using Infrastructure;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+public class ProgressController : APIControllerBase
+{
+    [Authorize]
+    [HttpGet]
+    public async Task<ActionResult<ICollection<HabitListDTO>>> GetProgress(
+        string UserId,
+        Guid HabitId,
+        CancellationToken cancellationToken)
+    {
+        ICollection<HabitProgressDTO> results = await Mediator.Send(
+            new GetHabitProgressQuery(
+                UserId,
+                HabitId),
+            cancellationToken);
+
+        return Ok(results);
+    }
+
+    [Authorize]
+    [HttpPost]
+    public async Task<ActionResult<ICollection<HabitProgressDTO>>> CompleteProgress(
+        string UserId,
+        Guid HabitId,
+        CancellationToken cancellationToken)
+    {
+        ICollection<HabitProgressDTO> results = await Mediator.Send(
+            new CompleteProgressCommand(
+                UserId,
+                HabitId),
+            cancellationToken);
+
+        return Ok(results);
+    }
+}
---
+++ api/src/Application/UseCases/Progress/Commands/CompleteProgressCommand.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using Domain.Models;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.UseCases.Progress.Commands;
+
+public record CompleteProgressCommand(
+    string UserId,
+    Guid HabitId) : IRequest<bool>;
+
+public class CompleteProgressCommandHanlder : IRequestHandler<CompleteProgressCommand, bool>
+{
+    private readonly IHabitTrackerDbContext _context;
+
+    public CompleteProgressCommandHanlder(IHabitTrackerDbContext context)
+        => _context = context;
+
+    public async Task<bool> Handle(
+        CompleteProgressCommand request,
+        CancellationToken cancellationToken)
+    {
+        var entity = new HabitProgress
+        {
+            Completed = true,
+            CompletedDate = DateTime.UtcNow,
+        };
+
+        Habit? habit = await _context.Habits
+            .Where(x => x.UserId == request.UserId)
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (habit is null)
+        {
+            return false;
+        }
+
+        habit.HabitProgresses.Add(entity);
+
+        await _context.SaveChangesAsync(cancellationToken);
+
+        return true;
+    }
+}
