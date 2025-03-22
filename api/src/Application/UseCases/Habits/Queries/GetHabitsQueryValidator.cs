7bbccf5c8180fa03151c9b502bf5e1d90108a1db
wade-loccisano <wade.loccisano@gmail.com>
2025-03-22 17:00:07 -0400

    add gethabitsquery validator.

---
+++ api/src/Application/UseCases/Habits/Queries/GetHabitsQueryValidator.cs
@@ -0,0 +1,11 @@
+﻿using Domain.UseCases.Habits;
+using FluentValidation;
+
+namespace Application.UseCases.Habits.Queries;
+
+public class GetHabitsQueryValidator : AbstractValidator<GetHabitsQuery>
+{
+    public GetHabitsQueryValidator()
+    {
+    }
+}
