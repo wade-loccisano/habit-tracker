5c866e378ad8ace32db9fc0c99543445e4065d04
wade-loccisano <wade.loccisano@gmail.com>
2025-03-27 23:18:07 -0400

    Add complete habit progress command tests.

---
+++ api/tests/API.UnitTests/UseCases/Progress/Commands/CompleteProgressCommandTests.cs
@@ -0,0 +1,37 @@
+﻿using Application.UseCases.Habits.Commands;
+using Application.UseCases.Progress.Commands;
+using Domain.Models;
+using FluentAssertions;
+using static API.UnitTests.Testing;
+
+namespace API.UnitTests.UseCases.Progress.Commands;
+
+public class CompleteProgressCommandTests : BaseTestFixture
+{
+    [Test]
+    public async Task ShouldCreateHabitProgress()
+    {
+        await RunAsDefaultUserAsync();
+        string userId = GetUserId() ?? string.Empty;
+        var habit = new Habit
+        {
+            UserId = userId,
+            Name = "Name",
+            Frequency = 12,
+            ReminderTime = DateTime.UtcNow,
+            StreakCount = 4,
+            HabitProgresses = { }
+        };
+        await AddAsync(habit);
+
+        var command = new CompleteProgressCommand(
+            userId,
+            habit.Id);
+
+        bool result = await SendAsync(command);
+
+        result.Should().BeTrue();
+        habit.StreakCount.Should().Be(1);
+        habit.HabitProgresses.Count.Should().Be(1);
+    }
+}
